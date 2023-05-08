547e8cf5dfe20b9744b9e33f548016829581a001
Arto Eskelinen <arto.eske@gmail.com>
2023-05-08 09:29:34 +0300

    lisäyksen testailua

--- toiminnot.js
+++ toiminnot.js
@@ -1,11 +1,23 @@
 let lomake = document.forms["kysely"];
+let lista = document.getElementById("nimiLista");
+
+lomake.addEventListener("submit", uusiKaveri);
+
+const kaverit = []
 
 function uusiKaveri(event){
     event.preventDefault()
     let nimi = document.querySelector('input[type="text"]').value;
     if(nimi.length < 1){
-        window.alert("Täytä tekstikenttä kaverisi nimellä.")
-        return
+        window.alert("Täytä tekstikenttä kaverisi nimellä.");
+        return;
     }
+    kaverit.push(nimi);
+    document.querySelector('input[type="text"]').value = "";
+
+    let uusiElementti = document.createElement("li");
+    let uusiElementtiNimi = document.createTextNode(nimi);
+    uusiElementti.appendChild(uusiElementtiNimi);
+    document.querySelector("#nimiLista").appendChild(uusiElementti);
 }
 
