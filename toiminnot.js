96cd7ef1195f1faac998aaa83127a067b4ef24e2
Arto Eskelinen <arto.eske@gmail.com>
2023-05-08 08:46:57 +0300

    lomakkeen toiminnallisuuden runkoa

--- toiminnot.js
+++ toiminnot.js
@@ -1,2 +1,11 @@
-let uusiKaveri = document.getElementById("nimi")
-console.log(uusiKaveri)
+let lomake = document.forms["kysely"];
+
+function uusiKaveri(event){
+    event.preventDefault()
+    let nimi = document.querySelector('input[type="text"]').value;
+    if(nimi.length < 1){
+        window.alert("Täytä tekstikenttä kaverisi nimellä.")
+        return
+    }
+}
+
